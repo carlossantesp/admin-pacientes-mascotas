a0f4455345bff5515c193e4fd958cb63e787fe22
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-03-10 15:08:01 -0400

    Listando las citas creadas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useState } from "react";
 import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
+
 function App() {
   const [citas, setCitas] = useState([]);
 
@@ -14,7 +16,12 @@ function App() {
           <div className="one-half column">
             <Formulario createCita={createCita} />
           </div>
-          <div className="one-half column">2</div>
+          <div className="one-half column">
+            <h2>Administra tus citas</h2>
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} />
+            ))}
+          </div>
         </div>
       </div>
     </Fragment>
